109d59be1baf075f3b2681f666d7a5ec95016e31
adamb924 <adamb924@gmail.com>
2019-09-03 10:45:21 -0500

    Prevent multiple instances

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <QApplication>
+#include <QSharedMemory>
+#include <QMessageBox>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    QSharedMemory sharedMemory;
+    sharedMemory.setKey("com.adambaker.taskmanager");
+
     QApplication a(argc, argv);
+
+    if (!sharedMemory.create(1))
+    {
+        QMessageBox::warning(nullptr, QObject::tr("Warning!"), QObject::tr("Task Manager is already running. Opening multiple instances of the program can result in your data being lost.") );
+        return 0;
+    }
+
     MainWindow w;
     w.show();
 
